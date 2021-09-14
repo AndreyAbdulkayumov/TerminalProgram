3e90d87656b45ce7b8c42bf595ec70c490c13c09
AndreyAbdulkayumov <50946238+MKprog128@users.noreply.github.com>
2021-09-14 21:32:33 +0300

    Добавьте файлы проекта.

---
+++ TerminalProgram/Device/ConnectedDevice.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TerminalProgram.Device
+{
+    public enum TypeOfConnection
+    {
+        NoDefined,
+        SerialPort,
+        Ethernet
+    }
+
+
+    
+
+    public class DataFromDevice : EventArgs
+    {
+        public string RX;
+    }
+
+    public class ConnectedDevice
+    {
+        /// <summary>
+        /// Переменная, хранящая в себе тип соединения с устройством.
+        /// </summary>
+        public TypeOfConnection SelectedConnection = TypeOfConnection.NoDefined;
+        
+        /// <summary>
+        /// Переменная, показывающая открыто ли соединение с устройством.
+        /// <para>true - соединение открыто.</para>
+        /// </summary>
+        public bool IsConnected { get; private set; } = false;
+
+        private SerialPort DeviceSerialPort = null;
+        private Socket DeviceLAN = null;
+
+        private const int TimeoutLANConnection = 1000; // В миллисекундах
+
+        public event EventHandler<DataFromDevice> SerialPortReceived;
+
+        public void Connect(string COM_Port, int BaudRate, string parity, int DataBits, string stopBits)
+        {
+            SerialPort_Connect(COM_Port, BaudRate, parity, DataBits, stopBits);
+            IsConnected = true;
+            SelectedConnection = TypeOfConnection.SerialPort;
+        }
+
+        public void Connect(string IPstr, int Port)
+        {
+            Ethernet_Connect(IPstr, Port);
+            IsConnected = true;
+            SelectedConnection = TypeOfConnection.Ethernet;
+        }
+        
+
+        public void Disconnect()
+        {
+            switch (SelectedConnection)
+            {
+                case TypeOfConnection.SerialPort:
+                    SerialPort_Disconnect();
+                    break;
+
+                case TypeOfConnection.Ethernet:
+                    Ethernet_Disconnect();
+                    break;
+
+                default:
+                    throw new Exception("Не задан тип соединения или выбран ранее неизвестный.");
+            }
+
+            IsConnected = false;
+            SelectedConnection = TypeOfConnection.NoDefined;
+
+        }
+
+        private void SerialPort_Connect(string COM_Port, int BaudRate, string parity, int DataBits, string stopBits)
+        {
+            DeviceSerialPort = new SerialPort();
+
+            Parity SelectedParity;
+
+            switch (parity)
+            {
+                case "None":
+                    SelectedParity = Parity.None;
+                    break;
+
+                case "Even":
+                    SelectedParity = Parity.Even;
+                    break;
+
+                case "Odd":
+                    SelectedParity = Parity.Odd;
+                    break;
+
+                default:
+                    throw new Exception("Неправильно задана четность у порта " + COM_Port + ".");
+            }
+
+            StopBits SelectedStopBits;
+
+            switch (stopBits)
+            {
+                case "0":
+                    SelectedStopBits = StopBits.None;
+                    break;
+
+                case "1":
+                    SelectedStopBits = StopBits.One;
+                    break;
+
+                case "1.5":
+                    SelectedStopBits = StopBits.OnePointFive;
+                    break;
+
+                case "2":
+                    SelectedStopBits = StopBits.Two;
+                    break;
+
+                default:
+                    throw new Exception("Неправильно задано колличество стоп - бит у порта " + COM_Port + ".");
+            }
+
+            try
+            {
+                DeviceSerialPort.PortName = COM_Port;
+                DeviceSerialPort.BaudRate = BaudRate;
+                DeviceSerialPort.Parity = SelectedParity;
+                DeviceSerialPort.DataBits = DataBits;
+                DeviceSerialPort.StopBits = SelectedStopBits;
+
+                DeviceSerialPort.Open();
+            }
+
+            catch (ArgumentException)
+            {
+                DeviceSerialPort.Close();
+                throw new Exception("Неправильно задано имя COM порта (Задано: " + COM_Port + ".");
+            }
+
+            catch (UnauthorizedAccessException)
+            {
+                DeviceSerialPort.Close();
+                throw new Exception("Отказ в доступе к порту (" + COM_Port + ").");
+            }
+
+            catch (InvalidOperationException)
+            {
+                throw new Exception("Этот порт (" + COM_Port + ") уже открыт.");
+            }
+
+            catch (IOException)
+            {
+                DeviceSerialPort.Close();
+                throw new Exception("Не удалось подключиться к прибору через " + COM_Port + ".");
+            }
+
+            catch (Exception error)
+            {
+                DeviceSerialPort.Close();
+                throw new Exception(error.Message);
+            }
+
+            DeviceSerialPort.DataReceived += DeviceSerialPort_DataReceived;
+        }
+
+        private void DeviceSerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (SerialPortReceived != null)
+            {
+                DataFromDevice Data = new DataFromDevice
+                {
+                    RX = DeviceSerialPort.ReadExisting()
+                };
+
+                SerialPortReceived(this, Data);
+            }
+        }
+
+        private void SerialPort_Disconnect()
+        {
+            if (DeviceSerialPort != null && DeviceSerialPort.IsOpen == true)
+            {
+                DeviceSerialPort.Close();
+                DeviceSerialPort = null;
+            }
+        }
+
+        private void Ethernet_Connect(string IPstr, int Port)
+        {
+            IPAddress IP = IPAddress.Parse(IPstr);
+            DeviceLAN = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IAsyncResult result = DeviceLAN.BeginConnect(IP, Port, null, null);
+
+            bool SuccessConnect = result.AsyncWaitHandle.WaitOne(TimeoutLANConnection, true);
+
+            if (SuccessConnect == true)
+            {
+                DeviceLAN.EndConnect(result);
+            }
+
+            else
+            {
+                Ethernet_Disconnect();
+                throw new Exception("Не удалось подключиться к устройству по Ethernet" +
+                    "\nЗаданный IP: " + IPstr +
+                    "\nЗаданный порт: " + Port.ToString());
+            }
+        }
+
+        private void Ethernet_Disconnect()
+        {
+            if (DeviceLAN != null)
+            {
+                DeviceLAN.Close();
+                DeviceLAN = null;
+            }
+        }
+
+
+        public void Send(ref char[] Data)
+        {
+            switch (SelectedConnection)
+            {
+                case TypeOfConnection.SerialPort:
+                    SerialPort_Send(ref Data);
+                    break;
+
+                case TypeOfConnection.Ethernet:
+                    Ethernet_Send(ref Data);
+                    break;
+
+                default:
+                    throw new Exception("Не задан тип соединения или выбран ранее неизвестный.");
+            }
+        }
+
+        private void SerialPort_Send(ref char[] Data)
+        {
+            DeviceSerialPort.Write(Data, 0, Data.Length);
+        }
+
+        private void Ethernet_Send(ref char[] Data)
+        {
+
+        }
+
+    }
+}
---
+++ TerminalProgram/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using TerminalProgram.Device;
+
+namespace TerminalProgram
+{
+    public struct ConnectionData
+    {
+        public string COMPort;
+        public string BaudRate;
+        public string Parity;
+        public string DataBits;
+        public string StopBits;
+    }
+
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConnectedDevice Device = new ConnectedDevice();
+        ConnectionData DeviceSettings = new ConnectionData();
+
+        readonly string[] ArrayBaudRate = { "4800", "9600", "19200", "38400", "57600", "115200" };
+        readonly string[] ArrayParity = { "None", "Even", "Odd" };
+        readonly string[] ArrayDataBits = { "8", "9" };
+        readonly string[] ArrayStopBits = { "0", "1", "1.5", "2" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RadioButton_SerialPort.IsChecked = true;
+
+            ComboBox_COMPort.AddHandler(ComboBox.MouseLeftButtonUpEvent, new MouseButtonEventHandler(ComboBox_MouseLeftButtonDown), true);
+            ComboBoxFilling(ComboBox_BaudRate, ref ArrayBaudRate);
+            ComboBoxFilling(ComboBox_Parity, ref ArrayParity);
+            ComboBoxFilling(ComboBox_DataBits, ref ArrayDataBits);
+            ComboBoxFilling(ComboBox_StopBits, ref ArrayStopBits);
+        }
+
+        private void ComboBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SearchSerialPorts();
+        }
+
+        private void SearchSerialPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            if (ports.Length != ComboBox_COMPort.Items.Count)
+            {
+                int CountItems = ComboBox_COMPort.Items.Count;
+
+                for (int i = 0; i < CountItems; i++)
+                {
+                    ComboBox_COMPort.Items.RemoveAt(0);
+                }
+
+                foreach (string port in ports)
+                {
+                    ComboBox_COMPort.Items.Add(port);
+                }
+            }
+        }
+
+        private void ComboBoxFilling(ComboBox Box, ref string[] Items)
+        {
+            for (int i = 0; i < Items.Length; i++)
+            {
+                Box.Items.Add(Items[i]);
+            }
+        }
+
+        private void SourceWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            SearchSerialPorts();
+        }
+             
+
+        private void MenuPreset_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuPreset_LoadMenuHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_SerialPort_Checked(object sender, RoutedEventArgs e)
+        {
+            TextBlock_COMPort.Visibility = Visibility.Visible;
+            ComboBox_COMPort.Visibility = Visibility.Visible;
+            TextBlock_BaudRate.Visibility = Visibility.Visible;
+            ComboBox_BaudRate.Visibility = Visibility.Visible;
+            TextBlock_Parity.Visibility = Visibility.Visible;
+            ComboBox_Parity.Visibility = Visibility.Visible;
+            TextBlock_DataBits.Visibility = Visibility.Visible;
+            ComboBox_DataBits.Visibility = Visibility.Visible;
+            TextBlock_StopBits.Visibility = Visibility.Visible;
+            ComboBox_StopBits.Visibility = Visibility.Visible;
+
+            TextBlock_IP.Visibility = Visibility.Hidden;
+            TextBox_IP.Visibility = Visibility.Hidden;
+            TextBlock_Port.Visibility = Visibility.Hidden;
+            TextBox_Port.Visibility = Visibility.Hidden;
+
+        }
+
+        private void RadioButton_Ethernet_Checked(object sender, RoutedEventArgs e)
+        {
+            TextBlock_COMPort.Visibility = Visibility.Hidden;
+            ComboBox_COMPort.Visibility = Visibility.Hidden;
+            TextBlock_BaudRate.Visibility = Visibility.Hidden;
+            ComboBox_BaudRate.Visibility = Visibility.Hidden;
+            TextBlock_Parity.Visibility = Visibility.Hidden;
+            ComboBox_Parity.Visibility = Visibility.Hidden;
+            TextBlock_DataBits.Visibility = Visibility.Hidden;
+            ComboBox_DataBits.Visibility = Visibility.Hidden;
+            TextBlock_StopBits.Visibility = Visibility.Hidden;
+            ComboBox_StopBits.Visibility = Visibility.Hidden;
+
+            TextBlock_IP.Visibility = Visibility.Visible;
+            TextBox_IP.Visibility = Visibility.Visible;
+            TextBlock_Port.Visibility = Visibility.Visible;
+            TextBox_Port.Visibility = Visibility.Visible;
+
+        }
+
+        private void TextBox_IP_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Port_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Connect_Click(object sender, RoutedEventArgs e)
+        {
+            Device.Connect(DeviceSettings.COMPort,
+                Convert.ToInt32(DeviceSettings.BaudRate),
+                DeviceSettings.Parity,
+                Convert.ToInt32(DeviceSettings.DataBits),
+                DeviceSettings.StopBits);
+
+            Device.SerialPortReceived += Device_SerialPortReceived;
+        }
+
+        private void Device_SerialPortReceived(object sender, DataFromDevice e)
+        {
+            TextBlock_RX.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                new Action(delegate () { TextBlock_RX.Text += e.RX; }));
+        }
+
+        private void Button_Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            Device.Disconnect();
+        }
+
+        private void TextBox_TX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            char[] Bytes = TextBox_TX.Text.ToCharArray();
+            Device.Send(ref Bytes);
+        }
+
+        private void ComboBox_COMPort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeviceSettings.COMPort = ComboBox_COMPort.SelectedItem?.ToString();
+        }
+
+        private void ComboBox_BaudRate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeviceSettings.BaudRate = ComboBox_BaudRate.SelectedItem.ToString();
+        }
+
+        private void ComboBox_Parity_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeviceSettings.Parity = ComboBox_Parity.SelectedItem.ToString();
+        }
+
+        private void ComboBox_DataBits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeviceSettings.DataBits = ComboBox_DataBits.SelectedItem.ToString();
+        }
+
+        private void ComboBox_StopBits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeviceSettings.StopBits = ComboBox_StopBits.SelectedItem.ToString();
+        }
+
+        
+    }
+}
