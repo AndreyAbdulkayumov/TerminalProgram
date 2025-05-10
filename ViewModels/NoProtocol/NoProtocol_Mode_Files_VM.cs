bc3472d6257182aae0a72ef842908a54f9bdeead
AndreyAbdulkayumov <86914394+AndreyAbdulkayumov@users.noreply.github.com>
2025-05-10 16:10:22 +0300

    Создано добавление/удаление файлов из специальной директории. В списочную фарму добавлен столбец с размером файла.

--- ViewModels/NoProtocol/NoProtocol_Mode_Files_VM.cs
+++ ViewModels/NoProtocol/NoProtocol_Mode_Files_VM.cs
@@ -1,4 +1,5 @@
-﻿using MessageBox_Core;
+﻿using Core.Models.Settings;
+using MessageBox_Core;
 using ReactiveUI;
 using Services.Interfaces;
 using System.Collections.ObjectModel;
@@ -21,26 +22,22 @@ namespace ViewModels.NoProtocol
 
         private readonly IFileSystemService _fileSystemService;
         private readonly IMessageBoxMainWindow _messageBox;
+        private readonly Model_Settings _settingsModel;
 
-        public NoProtocol_Mode_Files_VM(IFileSystemService fileSystemService, IMessageBoxMainWindow messageBox)
+        public NoProtocol_Mode_Files_VM(IFileSystemService fileSystemService, IMessageBoxMainWindow messageBox,
+            Model_Settings settingsModel)
         {
             _fileSystemService = fileSystemService ?? throw new ArgumentNullException(nameof(fileSystemService));
             _messageBox = messageBox ?? throw new ArgumentNullException(nameof(messageBox));
+            _settingsModel = settingsModel ?? throw new ArgumentNullException(nameof(settingsModel));
 
             Command_AddFile = ReactiveCommand.CreateFromTask(AddFileHandler);
             Command_AddFile.ThrownExceptions.Subscribe(error => _messageBox.Show($"Ошибка добавления файла.\n\n{error.Message}", MessageType.Error, error));
 
-            Command_RemoveAllFiles = ReactiveCommand.CreateFromTask(async () =>
-            {
-                if (Files.Count() == 0)
-                    return;
-
-                if (await messageBox.ShowYesNoDialog("Вы действительно хотите удалить все файлы?", MessageType.Warning) == MessageBoxResult.Yes)
-                {
-                    Files.Clear();
-                }
-            });
+            Command_RemoveAllFiles = ReactiveCommand.CreateFromTask(RemoveAllFiles);
             Command_RemoveAllFiles.ThrownExceptions.Subscribe(error => _messageBox.Show($"Ошибка удаления всех файлов.\n\n{error.Message}", MessageType.Error, error));
+
+            UpdateFileList();
         }
 
         private async Task AddFileHandler()
@@ -50,7 +47,25 @@ namespace ViewModels.NoProtocol
             if (string.IsNullOrEmpty(filePath))
                 return;
 
-            Files.Add(new SendFileItem_VM(Guid.NewGuid(), filePath, SendFileHandler, RemoveFileHandler, _messageBox));
+            string targetFilePath = _settingsModel.CopySendFileInWorkDirectory(filePath);
+
+            Files.Add(new SendFileItem_VM(Guid.NewGuid(), targetFilePath, SendFileHandler, RemoveFileHandler, _messageBox));
+        }
+
+        private async Task RemoveAllFiles()
+        {
+            if (Files.Count() == 0)
+                return;
+
+            if (await _messageBox.ShowYesNoDialog("Вы действительно хотите удалить все файлы?", MessageType.Warning) == MessageBoxResult.Yes)
+            {
+                foreach (var file in Files)
+                {
+                    _settingsModel.DeleteFile(file.FilePath);
+                }
+
+                Files.Clear();
+            }
         }
 
         private void SendFileHandler(Guid id)
@@ -62,7 +77,19 @@ namespace ViewModels.NoProtocol
         {
             var commandItem = Files.First(e => e.Id == id);
 
+            _settingsModel.DeleteFile(commandItem.FilePath);
+
             Files.Remove(commandItem);
+        }
+
+        private void UpdateFileList()
+        {
+            var files = _settingsModel.GetAllSendFilesNames();
+
+            foreach (var filePath in files)
+            {
+                Files.Add(new SendFileItem_VM(Guid.NewGuid(), filePath, SendFileHandler, RemoveFileHandler, _messageBox));
+            }
         }
     }
 }
--- ViewModels/NoProtocol/SendFileItem_VM.cs
+++ ViewModels/NoProtocol/SendFileItem_VM.cs
@@ -22,17 +22,28 @@ namespace ViewModels.NoProtocol
             set => this.RaiseAndSetIfChanged(ref _fileExtension, value);
         }
 
+        private string? _fileSize;
+
+        public string? FileSize
+        {
+            get => _fileSize;
+            set => this.RaiseAndSetIfChanged(ref _fileSize, value);
+        }
+
         public ReactiveCommand<Unit, Unit> Command_SendFile { get; }
         public ReactiveCommand<Unit, Unit> Command_RemoveFile { get; }
 
         public readonly Guid Id;
+        public readonly string FilePath;
 
         public SendFileItem_VM(Guid id, string filePath, Action<Guid> sendFileHandler, Action<Guid> removeFileHandler, IMessageBox messageBox)
         {
             Id = id;
+            FilePath = filePath;
 
             FileName = Path.GetFileNameWithoutExtension(filePath);
             FileExtension = Path.GetExtension(filePath);
+            FileSize = GetFileSize();
 
             Command_SendFile = ReactiveCommand.Create(() => sendFileHandler(id));
             Command_SendFile.ThrownExceptions.Subscribe(error => messageBox.Show($"Ошибка отправки файла \"{FileName}\".\n\n{error.Message}", MessageType.Error, error));
@@ -45,6 +56,29 @@ namespace ViewModels.NoProtocol
                 }
             });
             Command_RemoveFile.ThrownExceptions.Subscribe(error => messageBox.Show($"Ошибка удаления файла \"{FileName}\".\n\n{error.Message}", MessageType.Error, error));
+        }
+
+        private string GetFileSize()
+        {
+            var fileInfo = new FileInfo(FilePath);
+
+            var byteSize = fileInfo.Length;
+
+            const long KB = 1024;
+            const long MB = KB * 1024;
+            const long GB = MB * 1024;
+            const long TB = GB * 1024;
+
+            if (byteSize < KB)
+                return $"{byteSize} B";
+            else if (byteSize < MB)
+                return $"{(byteSize / (double)KB):F2} KB";
+            else if (byteSize < GB)
+                return $"{(byteSize / (double)MB):F2} MB";
+            else if (byteSize < TB)
+                return $"{(byteSize / (double)GB):F2} GB";
+            else
+                return $"{(byteSize / (double)TB):F2} TB";
         }
     }
 }
